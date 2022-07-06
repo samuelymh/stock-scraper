6f919293f523867a19c5a691b86b17172bd4acbe
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-07-07 01:15:48 +0800

    feat: Added more stub code for calculator component

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -7,9 +7,22 @@ import SearchBox from '../SearchBox/SearchBox';
 export default function Calculator() {
   const [stockTicker, setStockTicker] = useState('');
 
+  const fetchFinvizData = () => {
+    return 0;
+  }
+
+  const cleanFinvizData = () => {
+    return 0;
+  }
+
+  const setFinvizData = () => {
+    return 0;
+  }
+
+
   return (
     <div>
-      <SearchBox />
+      <SearchBox setStockTicker={setStockTicker}/>
       This is calculator and searchbox
     </div>
   );
