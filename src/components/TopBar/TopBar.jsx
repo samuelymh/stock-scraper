7b52c821079a22bf5db97830f17f4b33d9d65757
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-07-07 01:06:37 +0800

    chg: Redesign topbar

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -10,9 +10,9 @@ export default function TopBar() {
   return (
     <div 
       id='topbar' 
-      className='flex justify-between p-4 text-3xl shadow-lg'>
+      className='flex justify-between p-4 text-3xl'>
       <div 
-        className='transition ease-in-out hover:scale-110 border-4 p-1 w-1/4 border-[#7f5af0] border-opacity-75 rounded-md'>
+        className='transition ease-in-out hover:scale-110 p-1 my-auto w-1/4 rounded-md'>
         Scrapy
       </div>
       <Clock />
