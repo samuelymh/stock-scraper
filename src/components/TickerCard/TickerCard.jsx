2adc5a5e8a87056d5867aa98a03ccdb52e1c0811
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-07-17 21:34:50 +0800

    chg: Removed redundant code

--- src/components/TickerCard/TickerCard.jsx
+++ src/components/TickerCard/TickerCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TickerCard = ({ ticker }) => {
   return (
     <li className='m-2 transition ease-in-out hover:scale-110 hover:text-[#2cb67d]'>
-      {ticker.toLocaleUpperCase()}
+      {ticker}
     </li>
   );
 }
