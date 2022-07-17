9e20e0b05be63fc6c8c2913889380bb476ac85df
samuelymh <103726434+samuelymh@users.noreply.github.com>
2022-07-17 23:33:49 +0800

    feat: Implemented fetch call to request data from server

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
-// import puppeteer from 'puppeteer';
-// const puppeteer = require('puppeteer');
-
-// Components
 
 class Calculator extends Component {
-  FINVIZ_PAGE = "https://finviz.com/quote.ashx?t=MSFT";
+  constructor(props){
+    super(props);
+    this.state = {
+      finvizData: {}
+    }
+  }
+
+  shouldComponentUpdate(nextProps){
+    return nextProps.currentTicker !== this.props.currentTicker;
+  }
 
   fetchFinvizData = () => {
-    // async
-    return 0;
+    const { currentTicker } = this.props;
+    const { finvizData } = this.state;
+
+    // Make API call only if currentTicker is non-empty.
+    if(currentTicker){
+      console.log("Fetching data for " + currentTicker);
+      fetch('http://localhost:3001/finviz', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          ticker: currentTicker
+        })
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log("data:", {[currentTicker]: data});
+        // Set state with fetched data.
+        this.setState({ finvizData: {
+          ...finvizData,
+          [currentTicker]: data
+        } }, () => { console.log("finvizData:", this.state.finvizData) });
+      })
+      .catch(err => console.log(err));
+    }
   }
 
   cleanFinvizData = () => {
-    return 0;
-  }
-
-  setFinvizData = () => {
-    return 0;
+    this.setState({ finvizData: {} });
   }
 
   render(){
+    console.log('Rendering calculator');
+    this.fetchFinvizData();
     return (
       <div className='border-4 lg:w-3/5 w-full grow my-10 mx-5 rounded-md'>
         <p>Calculator</p>
